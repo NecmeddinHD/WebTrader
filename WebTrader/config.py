a00d93a80b26fcd1942615029ef6de0d32196e20
NecmeddinHD <necmeddingencanhd@gmail.com>
2025-04-11 09:57:47 +0300

    Add files via upload

---
+++ WebTrader/config.py
@@ -0,0 +1,9 @@
+﻿# TR  Chat idnizi @userinfobot telegram botu aracılığıyla öğrenebilirsiniz.
+# EN  You can find your chat id via the @userinfobot Telegram bot.
+# ES  Puedes obtener tu ID de chat a través del bot de Telegram @userinfobot.
+# FR  Vous pouvez obtenir votre ID de chat via le bot Telegram @userinfobot.
+# DE  Sie können Ihre Chat-ID über den Telegram-Bot @userinfobot erhalten.
+# IT  Puoi ottenere il tuo ID chat tramite il bot Telegram @userinfobot.
+# PT  Você pode obter seu ID de chat através do bot Telegram @userinfobot.
+# RU  Вы можете получить свой ID чата через Telegram-бота @userinfobot.
+chatid = "YOUR CHAT ID"
